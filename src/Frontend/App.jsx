2bcfb1546801abfa29b1e7b33ffa34e347d8d47a
Itsindi22 <inderpreet2004123@gmail.com>
2026-02-08 08:38:53 -0500

    wip

--- src/Frontend/App.jsx
+++ src/Frontend/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import FindAPet from "./pages/findAPet";
+import Home from "../Frontend/pages/home";
+import FindAPet from "../Frontend/pages/findAPet";
 
 export default function App() {
   return (
